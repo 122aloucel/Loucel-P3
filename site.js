e0a38c9087598707fa3b820fb0777ae7d47d770f
122aloucel <122aloucel@frhsd.com>
2022-04-01 13:40:09 -0400

    finished posterclick

--- site.js
+++ site.js
@@ -75,10 +75,17 @@ const vue_app = Vue.createApp({
               return month + " " + dateArray[2] + " " + dateArray[1];
             },
             posterClick(index) {
-              this.movies[index].posterindex++;
+              if (this.movies[index].posterindex >= this.movies[index].posters.length-1) {
+                this.movies[index].posterindex=0;
+              }
+              else {
+                this.movies[index].posterindex++;
+              }
 
             },
-            
+            timeText(minutes) {
+
+            }
       }
 })
 
